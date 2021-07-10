cda30aea8c6e959c907f34240bff655517e7fa8f
tomoh88 <tomoh88.wiki@gmail.com>
2021-07-10 23:14:31 +0900

    5번 연습

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,51 +1,28 @@
 import React from 'react';
-import PropType from 'prop-types';
 
-function Food({name,picture,rating}) {
-  return (
-  <div> 
-    <h2>I love {name}</h2>
-    <h4>{rating} / 5.0 </h4>
-    <img src={picture} alt={name} />
-  </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count : current.count + 1 ,
+    }));
+  };
+  mimus = () => {
+    this.setState(current => ({
+      count: current.count -1,
+    }));
+  };
+  render(){
+    return (
+    <div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>add</button>
+      <button onClick={this.mimus}>mimus</button>
+    </div>
+    );
+   }
 }
-
-const foodILike = [
-{
-  id: 1,
-  name: 'Ayaka',
-  image: 'https://pbs.twimg.com/media/E3ReemDUYAEehZM.jpg',
-  rating: 5,
-},
-{
-  id: 2,
-  name: 'Yoimiya',
-  image: 'https://pbs.twimg.com/media/E3RZNCjVkAYrgw0.jpg',
-  rating: 4.9,
-}
-];
-
-// function renderFood(dish) {
-//   return <Food name={dish.name} picture={dish.picture}/>;
-// }
-
-function App() {
-  return (
-  <div>
-    Hello world!!!
-    {
-    foodILike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-    ))}
-  </div>
-  );
-}
-
-Food.propType = {
-  name: PropType.string.isRequired,
-  picture: PropType.string.isRequired,
-  rating: PropType.number,
-};
-
 export default App;
