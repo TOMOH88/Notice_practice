15d6c3119ee3083cb98d709d15ccb6d8b205ee82
tomoh88 <tomoh88.wiki@gmail.com>
2021-07-11 15:41:29 +0900

    8장 종료?

--- movie_app_2021/src/routes/Detail.js
+++ movie_app_2021/src/routes/Detail.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import './Detail.css';
 
-function Detail(props) {
-    console.log(props);
-    return <span>Hello!</span>
+// function Detail(props) {
+//     console.log(props);
+//     return <span>Hello!</span>
+// }
+
+
+class Detail extends React.Component{
+    componentDidMount() {
+        const { location, history }= this.props;
+        if(location.state == undefined ){
+            history.push('/');
+        }
+    }
+
+    render() {
+        const { location, history }= this.props;
+        if (location.state) {
+        return (
+            <span>{ location.state.title }</span>
+
+
+
+        ); 
+        } else {
+            return null;
+        }
+    }
 }
-
 export default Detail;
