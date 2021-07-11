64b1e11e1e2677cc73e9ff5f07e3b148dbaf30b9
tomoh88 <tomoh88.wiki@gmail.com>
2021-07-11 16:05:03 +0900

    마무리

--- movie_app_2021/src/routes/Detail.js
+++ movie_app_2021/src/routes/Detail.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './Detail.css';
 
-// function Detail(props) {
-//     console.log(props);
-//     return <span>Hello!</span>
-// }
-
-
 class Detail extends React.Component{
     componentDidMount() {
         const { location, history }= this.props;
@@ -19,10 +13,21 @@ class Detail extends React.Component{
         const { location, history }= this.props;
         if (location.state) {
         return (
-            <span>{ location.state.title }</span>
 
+            <div className="detail"> 
+            <img src={location.state.poster} alt={location.state.title} title={location.state.title}/>
+            <div className="detail__data">
+                <h3 className="detail__title">{location.state.title}</h3>
+                <h5 className="detail__year">{location.state.year}</h5>
+                <ul className="detail__genres">
+                    {location.state.genres.map((genre, index) => {
+                        return <li key={index} className="detail__genre">{genre}</li>;
+                    })}
+                </ul>
+                <p className="detail__summary">{location.state.summary}</p>
+            </div>
 
-
+        </div>
         ); 
         } else {
             return null;
